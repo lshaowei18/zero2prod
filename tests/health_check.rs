1498a5181bdee32c28dab9d4a3e9b1afe8f7fb42
Shao Wei <lshaowei18@gmail.com>
2021-03-13 12:59:15 +0800

    chore: tests for /subscriptions

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,6 +4,8 @@ Spares me from having to specify the `#[test]` attribute.
 */
 use std::net::TcpListener;
 
+const FORM_HEADER: &str = "application/x-www-form-urlencoded";
+
 #[actix_rt::test]
 async fn health_check_works() {
     let address = spawn_app().await;
@@ -19,6 +21,57 @@ async fn health_check_works() {
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app().await;
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    // Act
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", FORM_HEADER)
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", FORM_HEADER)
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        )
+    }
 }
 
 async fn spawn_app() -> String {
